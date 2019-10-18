e9525e936e46699364ed55d3ed26c08730ddeaf6
snowdream <yanghui1986527@gmail.com>
2019-10-18 15:41:55 +0800

    Add: config for vue and cordova

--- vue-app/config/index.js
+++ vue-app/config/index.js
@@ -45,12 +45,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../cordova-app/www/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsRoot: path.resolve(__dirname, '../../cordova-app/www'),
+    assetsSubDirectory: '',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
