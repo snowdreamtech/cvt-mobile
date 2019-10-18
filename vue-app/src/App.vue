dc23c306b85b472d63becf2728f4af33930c52b2
snowdream <yanghui1986527@gmail.com>
2019-10-18 23:03:50 +0800

    Add: vant from https://github.com/youzan/vant

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <van-button type="primary">主要按钮</van-button>
+
+    <router-view />
   </div>
 </template>
 
 <script>
+import { Button } from 'vant'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    [Button.name]: Button
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
