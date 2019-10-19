0e3a7db465741afbd7437a36de4d9d27404823f0
snowdream <yanghui1986527@gmail.com>
2019-10-19 09:46:14 +0800

    Add: fastclick from https://www.jianshu.com/p/bec7dbd5cdc6

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,8 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import FastClick from 'fastclick'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+FastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
@@ -10,6 +13,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
